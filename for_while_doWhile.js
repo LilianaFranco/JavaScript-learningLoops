24db54dd0c4443e48e2bbd9c2dcc5aea3df96de7
LilianaFranco <franco0123@gmail.com>
2022-03-20 20:31:57 -0500

    Ejercicios de integracion 18 de marzo

--- for_while_doWhile.js
+++ for_while_doWhile.js
@@ -3,7 +3,7 @@ function giveMeTen(number){
     /**
     * Adivinar los siguientes 10 números a una cifra dada
     * @param  number {int} un número para iniciar.
-    * @return {int} imprime en pantalla los sigueintes 10 números que siguen a la cifra dada
+    * @return {int} imprime en pantalla los siguientes 10 números que siguen a la cifra dada
     */
     let i = 0;
     let secuencia = number;
@@ -22,7 +22,7 @@ function contarTresEnTres(min, max){
     //* cuenta de 3 en tres desde una cifra dada para iniciar, hasta una cifra dada para detenerse
     //* @param  min {int} un número desde donde debe empezarse a contar
     //* @param  max {int} un número hasta donde se debe contar
-    //* @return {int} imprime en pantalla los numeros de tres en tres hasta el número máximo
+    //* @return {int} imprime en pantalla los números de tres en tres hasta el número máximo
     //*/
     let i=min;
     let contador = min;
@@ -38,17 +38,27 @@ console.log(cifrasTresEnTres);
 
 //Ejercicio 3
 function sumarConsecutivos(number){
-    let i = number;
-    let resultado = 1;
-    for (i=number; i>0; i--)
+    /**
+    * Suma todos los numeros consecutivos hasta llegar al número máximo entregado
+    * @param  number {int} un número para terminar
+    * @return {int} la suma de los números
+    */
+    let i = 1;
+    let resultado = 0;
+    for (i=1; i<=number; i++)
     resultado = resultado + i;
     return resultado
 }
-let sumaConsecutiva = sumarConsecutivos(5);
+let sumaConsecutiva = sumarConsecutivos(100);
 console.log(sumaConsecutiva)
 
 //Ejercicio 4
 function factorial(number){
+    /**
+    * Dado un numero, multiplica todos los números que senencuentra hasta llegar a 1
+    * @param  number {int} un número para iniciar.
+    * @return {int} imprime en pantalla la multiplicaicón de todos los números 
+    */
     let i = number;
     let resultado = 1;
     for (i=number; i>0; i--)
@@ -61,6 +71,14 @@ console.log(conteo)
 
 //Ejercicio 5
 function generateFibonacci(number){
+    /**
+    * Dado un número entero positivo, mostrar su factorial. El factorial de
+        un número se obtiene multiplicando todos los números enteros
+        positivos que hay entre el 1 y ese número.
+    * @param  number {int} un número para terminar
+    * @return {int} imprime en pantalla todos los números fibonacci encontrados 
+        desde 0 hasta le número dado por parámetro
+    */
     let anterior = 0
     let siguiente = 1
     let fibonacci = 0
--- nuevosEjercicios.js
+++ nuevosEjercicios.js
@@ -1,6 +1,6 @@
 const prompt = require ('prompt-sync')({sigint: true})
 
-// Ejercicio 1
+// // Ejercicio 1
 function imprimirFrase(){
     /**
     * Pide frase al usuario frase y la imprime en pantalla.
@@ -11,7 +11,7 @@ function imprimirFrase(){
 }
 imprimirFrase();
 
-// Ejercicio 2
+// // Ejercicio 2
 function saludar(){
     /**
     * Pide nombre al usuario frase y lo saluda por su nombre.
@@ -22,7 +22,7 @@ function saludar(){
 }
 saludar();
 
-// Ejercicio 3
+// // Ejercicio 3
 function sumar(){
     /**
     * Pide dos números al usuario y los imprime en pantalla.
@@ -34,7 +34,7 @@ function sumar(){
 }
 sumar();
 
-// Ejercicio 4
+// // Ejercicio 4
 function calcularEdad(){
     /**
     * @parameter {int} Pide al usuario un número de base.
@@ -46,7 +46,7 @@ function calcularEdad(){
 }
 calcularEdad();
 
-// Ejercicio 5
+// // Ejercicio 5
 function getRandomInt(min, max) {
     /**
     * Pide frase al usuario frase y la imprime en pantalla.
@@ -55,7 +55,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function guessTheNumber() {
+// function guessTheNumber() {
     /**
     * Compara un numero entregado con un número aleatorio de 1 a 10
     * @return esNumeroSecretro {string} imprime en pantalla felicitaciones si adivíno o informa si se equivocó.
@@ -70,8 +70,135 @@ function guessTheNumber() {
     }
 }
 
-console.log (guessTheNumber(3))
+// console.log (guessTheNumber(3))
 
 // Ejercicio 6
+function encontrarPares(max){
+    /**
+    //* encuentra todos los pares dsde 0 hasta hasta una cifra dada para detenerse.
+    //* @param  max {int} un número hasta donde se debe contar de dos en dos
+    //* @return {int} imprime en pantalla los numeros pares desde 0 hasta el número máximo
+    //*/
+    let contador = 0;
+    let i = 0
+    do{
+        contador = contador + 2;
+        console.log(contador)
+        i= i+2;
+    }
+    while (i < max);
+}
+let pares = encontrarPares(100);
+console.log(pares);
 
+//Ejercicio 7
+function recorrerArreglo(nombres){
+    /**
+    //* Imprime todos los elementos de un arreglo
+    //* @param  nombres {array} arreglo ingresado por el usuario
+    //* @return {int} imprime en pantalla los elementos de un arreglo
+    //*/
+    let i = 0
+    for (i=0; i<nombres.length; i++){
+        console.log(i)
+        console.log(nombres[i])
+        
+    }
+}
+console.log(recorrerArreglo(["Pedro", "Pablo", "Maria", "Juan", "Diana"]))
 
+//Ejercicio 8
+function percentage(number1, number2){
+    /**
+    * Calculate the percentage of one number
+    * @param  number1 {int} the percentage we want to calculate
+    * @param  number2 {int} a given number
+    * @return result {float} the percentage number2 of a given number1.
+    */
+    let result = (number1*number2)/100
+    return result
+}
+function percentageIf(number){
+    if (number>500){
+        return percentage(18, number)
+    }
+}
+console.log(percentageIf(650))
+
+//Ejercicio 9
+function returnElementThree(nombres){
+    /**
+    //* Imprime el elemento 3 de un arreglo, sino lo encuentra retorna -1
+    //* @param  nombres {array} arreglo ingresado por el usuario
+    //* @return {int} imprime en pantalla el elemento de l posición 3 o -1 si no existe dicha posición.
+    //*/
+    if(nombres[3]){
+        return nombres[3]
+    }
+    else
+        return -1
+    
+}
+
+let notas = [3,8,9,10]
+let edad = [3,8,9]
+console.log(returnElementThree(notas))
+console.log(returnElementThree(edad))
+
+Ejercicio 10
+function twoOrThreeDigits(){
+    let number = prompt("Ingrese un número de máximo dos cifras: ")
+    if (number>99){
+        return "Error. Ingresa un número de máximo dos cifras"
+    }
+    else if (number>=0){
+        if(number < 10){
+            cifraFaltante = 10 - number
+            return `Para ser un número de dos cifras te faltan ${-cifraFaltante} unidades`
+        }
+        else{
+        cifraFaltante = 100 - number
+        return `Para ser un número de tres cifras te faltan ${-cifraFaltante} unidades`
+        }
+    }
+    else {
+        if(number > -10){
+            cifraFaltante = (10 + +number) * -1
+            return `Para ser un número de dos cifras te faltan ${cifraFaltante} unidades`
+        }
+        else{
+        cifraFaltante = (100 + +number) * -1
+        return `Para ser un número de tres cifras te faltan ${cifraFaltante} unidades`
+        }
+    }
+}
+
+console.log(twoOrThreeDigits())
+
+//Ejercicio 11
+function averageNotes(notes){
+    let numberOfNotes = notes.length
+    let suma = 0
+    for (let i=0; i<numberOfNotes; i++){
+        suma = suma + notes[i]
+    }
+    return suma/numberOfNotes
+}
+
+console.log(averageNotes([5,5,5,4]))
+
+//Ejercicio 12
+function sumarConsecutivos(number){
+    /**
+    * Suma todos los numeros consecutivos hasta llegar al número máximo entregado
+    * @param  number {int} un número para terminar
+    * @return {int} la suma de los números
+    */
+    let i = 1;
+    let resultado = 0;
+    for (i=1; i<=number; i++)
+    resultado = resultado + i;
+    return resultado
+}
+let sumaConsecutiva = sumarConsecutivos(100);
+console.log(sumaConsecutiva)
